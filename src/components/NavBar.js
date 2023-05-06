0e85a36c9aea963cc4afed02d37327986eea422a
Yago Hilario <gityago@gmail.com>
2023-05-06 17:55:36 -0300

    corrigindo o menu responsivo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,20 +3,33 @@ import './NavBar.css'
 import { NavLink} from 'react-router-dom'
 
 function NavBar() {
+
+    const btnMobile = document.getElementById('btn-mobile')
+
+    function  handleClick() {
+        const nav = document.getElementById('nav')
+        nav.classList.toggle('active')
+    }
+
   return (
-    <nav>
-        <ul>
-            <li>
-                <NavLink to='/'>Home</NavLink>
-            </li>
-            <li>
-                <NavLink to='/habilidades'>Habilidades</NavLink>
-            </li>
-            <li>
-                <NavLink to='/projeto'>Projetos</NavLink>
-            </li>
-        </ul>
-    </nav>
+   <header id='header'>
+        <nav id='nav'>
+            <button id='btn-mobile' onClick={handleClick}>
+                <span id='hamburguer'></span>
+            </button>
+            <ul id='menu'>
+                <li>
+                    <NavLink to='/'>Home</NavLink>
+                </li>
+                <li>
+                    <NavLink to='/habilidades'>Habilidades</NavLink>
+                </li>
+                <li>
+                    <NavLink to='/projeto'>Projetos</NavLink>
+                </li>
+            </ul>
+        </nav>
+   </header>
   )
 }
 
